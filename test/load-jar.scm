696205c8525c9f8b6a2cf643413275c17d80b51e
rrm <github@5ess.net>
2009-06-26 19:38:48 -0400

    additional test for loading scheme code from external jar files via load-jar

--- test/load-jar.scm
+++ test/load-jar.scm
@@ -1,11 +1,17 @@
 (use 'test)
 
+; load the test jar file
 (load-jar "test/test.jar")
 
-(define test-1 (method "test.Test" "test1"))
-
 (define (test:load-jar)
-  (check (test-1) => 1))
+ (let ((test-1 (method "test.Test" "test1")))
+  ; call test method on java class
+  (check (test-1) => 1)
+  ; load scheme code from jar file
+  (load "testfun.scm")
+  ; call the loaded function
+  (check (testfun #t) => #t)))
 
 (test:load-jar)
+
 (check-report)
