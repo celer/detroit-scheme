56d83b7326fbba8c9dba29610f3be6ddb02877d7
rrm <github@5ess.net>
2009-08-25 00:29:46 -0400

    test:unit

--- src/lib/test.scm
+++ src/lib/test.scm
@@ -1,2 +1,7 @@
 ; Copyright (c) 2009, Raymond R. Medeiros. All rights reserved.
 (srfi 78)
+
+; run a unit test 
+(define (test:unit name)
+  (require (string->symbol (conc "test/" (symbol->string name)))))
+
