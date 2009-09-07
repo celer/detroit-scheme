39aa0efeff2c6148ca5e3a24c39fd034dfc1b6f1
rm <rm@wb.localdomain>
2009-09-07 02:21:28 -0400

    macro for test:unit

--- src/lib/test.scm
+++ src/lib/test.scm
@@ -2,6 +2,9 @@
 (srfi 78)
 
 ; run a unit test 
-(define (test:unit name)
-  (require (string->symbol (conc "test/" (symbol->string name)))))
-
+(define-macro
+  (test:unit . tests)
+  (for-each
+    (lambda (test)
+      (require (string->symbol (conc "test/" (symbol->string test)))))
+    tests))
